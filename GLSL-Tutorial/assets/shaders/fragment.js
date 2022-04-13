daf7317186f4098c428ce3b4e4e11e52a69b567f
= <zainr7989@gmail.com>
2022-04-13 16:26:10 -0400

    Add noise to our geometry.

--- GLSL-Tutorial/assets/shaders/fragment.js
+++ GLSL-Tutorial/assets/shaders/fragment.js
@@ -1,5 +1,7 @@
 export const fragmentShader = `
+    varying float vDistortion;
+
     void main() {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        gl_FragColor = vec4(vDistortion, vDistortion, vDistortion, 1.0);
     }
 `;
