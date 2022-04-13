0fc5400f27eb121d076e5ba174569d7d9700c50a
= <zainr7989@gmail.com>
2022-04-13 12:23:28 -0400

    Apply red shading, using a wave-like shape.

--- GLSL-Tutorial/assets/shaders/vertex.js
+++ GLSL-Tutorial/assets/shaders/vertex.js
@@ -1,7 +1,11 @@
 export const vertexShader = `
     void main() {
-    vec3 transPosition = position.xyz; 
+        vec3 transPosition = position;
 
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(transPosition, 1.0);
+        // set the z-coords to look wavy
+        float distortion = sin(transPosition.x * 5.0);
+        transPosition.z = transPosition.z + distortion;
+
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(transPosition, 1.0);
     }
 `;
