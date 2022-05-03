bf922270d23d449d37e3316b0c27a247212e5bd0
= <zainr7989@gmail.com>
2022-05-03 12:20:43 -0400

    create a WebGL triangle.

--- InventBox-WebGL/main.js
+++ InventBox-WebGL/main.js
@@ -25,10 +25,43 @@ gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
 
 // create vertex shader
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertexShader, `
+    attribute vec3 position;
+    void main() {
+        gl_Position = vec4(position, 1);
+    }
+`);
+gl.compileShader(vertexShader);
+
 // create fragment shader 
+const fragmentShader = gl.createShader(gl.FRAGENT_SHADER);
+gl.shaderSource(fragmentShader, `
+    void main() {
+        gl_FragColor = vec4(1, 0, 0, 1); // this is hardcoding a color across ALL pixels
+    }
+`);
+gl.compileShader();
+
 // create program
+const program = gl.createProgram();
 // attach shaders to program
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+gl.linkProgram(program); // ???
 
-// enable vertex attrs
+// enable vertex attrs - in this case, we just have position
+const positionLocation = gl.getAttribLocation(program, `position`);  // the string comes from our attribute in the GLSL above!
+gl.enableVertexAttribArray(positionLocation);  // attrs are disabled by default 
 
+/**
+ * the '3' tells WebGL that each position takes 3 values
+ * dtype comes from the Float32Array above  
+ * no normalization for now (more so for optimization)  
+ * no stride, 
+ * no offset
+ */
+gl.vertexAttribPointer(positionLocation, 3, gl.Float, false, 0, 0);
+
+gl.useProgram(program); // creates an exec on the GPU
 // draw!!
+gl.drawArrays(gl.TRIANGLES, 0, 3);
